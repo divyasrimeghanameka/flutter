57b935fd9b90ba9fe6e9e6a424a0528a3eb46137
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2025-10-14 14:16:23 +0530

    Create 4a_internal.dart

---
+++ 4a_internal.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigator Example',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+
+          
+          child: ElevatedButton(
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),
+      child: const Text('Go to Second Screen'),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SecondScreen()),
+        );
+      },         ),       ),
+    );  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),
+          child: const Text('Go Back'),
+          onPressed: () {
+            Navigator.pop(context); // Go back to HomeScreen
+          },
+        ),      ),    );
+  }
+}
